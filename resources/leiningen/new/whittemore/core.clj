7360380a79b914704605cdd185a10864449db80a
jtcb <jbrule@cs.umd.edu>
2018-12-10 17:42:26 -0500

    Copy files from 'default' template

--- resources/leiningen/new/whittemore/core.clj
+++ resources/leiningen/new/whittemore/core.clj
@@ -1,1 +1,6 @@
-(def {{name}} :foo)
+(ns {{namespace}})
+
+(defn foo
+  "I don't do a whole lot."
+  [x]
+  (println x "Hello, World!"))
--- src/leiningen/new/whittemore.clj
+++ src/leiningen/new/whittemore.clj
@@ -1,5 +1,7 @@
 (ns leiningen.new.whittemore
-  (:require [leiningen.new.templates :refer [renderer name-to-path ->files]]
+  (:require [leiningen.new.templates :refer [renderer year date project-name
+                                             ->files sanitize-ns name-to-path
+                                             multi-segment]]
             [leiningen.core.main :as main]))
 
 (def render (renderer "whittemore"))
@@ -7,8 +9,24 @@
 (defn whittemore
   "Create a new Whittemore project"
   [name]
-  (let [data {:name name
-              :sanitized (name-to-path name)}]
-    (main/info "Generating fresh 'lein new' whittemore project.")
+  (let [render (renderer "default")
+        main-ns (multi-segment (sanitize-ns name))
+        data {:raw-name name
+              :name (project-name name)
+              :namespace main-ns
+              :nested-dirs (name-to-path main-ns)
+              :year (year)
+              :date (date)}]
+    (main/info "Generating a project called" name
+               "based on the 'whittemore' template.")
     (->files data
-             ["src/{{sanitized}}/core.clj" (render "core.clj" data)])))
+             ["project.clj" (render "project.clj" data)]
+             ["README.md" (render "README.md" data)]
+             [".gitignore" (render "gitignore" data)]
+             [".hgignore" (render "hgignore" data)]
+             ["src/{{nested-dirs}}.clj" (render "core.clj" data)]
+             ["test/{{nested-dirs}}_test.clj" (render "test.clj" data)]
+             ["LICENSE" (render "LICENSE" data)]
+             "data"
+             "notebooks")))
+
