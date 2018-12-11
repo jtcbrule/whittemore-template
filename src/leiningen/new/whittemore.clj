c7af5582739587498b2ee078f1014c60e0f93fc5
jtcb <jbrule@cs.umd.edu>
2018-12-11 17:00:59 -0500

    Add example data and notebook to template

--- src/leiningen/new/whittemore.clj
+++ src/leiningen/new/whittemore.clj
@@ -1,13 +1,14 @@
 (ns leiningen.new.whittemore
   (:require [leiningen.new.templates :refer [renderer year date project-name
                                              ->files sanitize-ns name-to-path
-                                             multi-segment]]
+                                             multi-segment raw-resourcer]]
             [leiningen.core.main :as main]))
 
 (defn whittemore
   "Create a new Whittemore project"
   [name]
   (let [render (renderer "whittemore")
+        raw (raw-resourcer "whittemore")
         main-ns (multi-segment (sanitize-ns name))
         data {:raw-name name
               :name (project-name name)
@@ -25,6 +26,6 @@
              ["src/{{nested-dirs}}.clj" (render "core.clj" data)]
              ["test/{{nested-dirs}}_test.clj" (render "test.clj" data)]
              ["LICENSE" (render "LICENSE" data)]
-             "data"
-             "notebooks")))
+             ["data/smoking.csv" (render "smoking.csv" data)]
+             ["notebooks/front_door.ipynb" (raw "front_door.ipynb")])))
 
